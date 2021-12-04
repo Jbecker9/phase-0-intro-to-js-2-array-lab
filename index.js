6b49133559aaf213142800b8a4ff29ad305c2442
Jared <jbeckersalax9@aol.com>
2021-12-04 16:40:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+};
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo');
+}
+
+
+
+
